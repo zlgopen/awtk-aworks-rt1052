48ab1bbf9ed6ddfbbfe8c558058459885352f060
xuchaoze <xuchaoze@zlg.cn>
2018-10-17 18:33:27 +0800

    update copy_files.py and docs

--- README.md
+++ README.md
@@ -4,54 +4,89 @@
 
 [awtk-aworks-rt1052](https://github.com/zlgopen/awtk-aworks-rt1052)是AWTK在AWorks(RT1052)上的移植。
 
-## 编译
+## 一、环境配置
 
-1. 将awtk取到当前目录
+#### 1.1 克隆(clone)  awtk
 
 ```
 git clone https://github.com/zlgopen/awtk.git
 ```
 
-2. 拷贝源文件到Aworks SDK中(请修改copy\_files.py指明Aworks SDK路径, SDK路不支持中文)。
+#### 1.2 修改 copy\_files.py
+
+  主要有如下两项：
+
+- 设置AWTK_ROOT_DIR（即awtk所在目录）；
+
+
+- 设置DST_ROOT_DIR（即输出目录）。一般该目录为AWorks工程目录, 不支持中文。
+
+#### 1.3 执行 copy\_files.py
 
 ```
 python copy_files.py
 ```
 
-3. 在eclipse中定义宏: HAS\_AWTK\_CONFIG
+#### 1.4 添加预定义宏HAS\_AWTK\_CONFIG
 
-4. 在eclipse中增加下列路径：
+  有两种方式：
 
-```
-"${workspace_loc:/${ProjName}/awtk/}"
-"${workspace_loc:/${ProjName}/awtk/src}"
-"${workspace_loc:/${ProjName}/awtk/src/ext_widgets}"
-"${workspace_loc:/${ProjName}/awtk/3rd}"
-"${workspace_loc:/${ProjName}/awtk/3rd/agge}"
-"${workspace_loc:/${ProjName}/awtk/3rd/nanovg}"
-"${workspace_loc:/${ProjName}/awtk/3rd/nanovg/base}"
-"${workspace_loc:/${ProjName}/awtk/3rd/gpinyin/include}"
-"${workspace_loc:/${ProjName}/awtk/3rd/libunibreak}"
-"${workspace_loc:/${ProjName}/awtk-port}"
-"${workspace_loc:/${ProjName}/awtk-demos}"
-"${workspace_loc:/${ProjName}/pxp}"
-"${workspace_loc:/${ProjName}/app}"
-```
+1. 进入eclipse, 导入并选中AWorks工程，在如下两个地方添加宏：
 
-或者直接修改.cproject文件，在相应位置加入：
+   - "Project"菜单项 -> "C/C++ Build" -> "Settings" -> "Tool Settings"标签页 -> "Cross ARM C Compiler" -> "Preprocessor"；
+
+   - "Project"菜单项 -> "C/C++ Build" -> "Settings" -> "Tool Settings"标签页 -> "Cross ARM C++ Compiler" -> "Preprocessor"
+
+2. 直接修改AWorks工程的.cproject文件
+
+   找到所有 name="Cross ARM C Compiler"或者name="Cross ARM C++ Compiler"的tool元素，在name="Defined symbols (-D)"的option子元素中加入：
+
+   ```
+   <listOptionValue builtIn="false" value="HAS_AWTK_CONFIG"/>
+   ```
+
+#### 1.5 添加awtk相关的头文件路径
+
+  有两种方式：
+
+1. 进入eclipse, 导入并选中AWorks工程，在如下两个地方添加路径：
+
+   - "Project"菜单项 -> "C/C++ Build" -> "Settings" -> "Tool Settings"标签页 -> "Cross ARM C Compiler" -> "Includes"；
+
+   - "Project"菜单项 -> "C/C++ Build" -> "Settings" -> "Tool Settings"标签页 -> "Cross ARM C++ Compiler" -> "Includes"
+
+   awtk相关的包含路径如下：
+
+   ```
+   "${workspace_loc:/${ProjName}/awtk/}"
+   "${workspace_loc:/${ProjName}/awtk/src}"
+   "${workspace_loc:/${ProjName}/awtk/src/ext_widgets}"
+   "${workspace_loc:/${ProjName}/awtk/3rd}"
+   "${workspace_loc:/${ProjName}/awtk/3rd/agge}"
+   "${workspace_loc:/${ProjName}/awtk/3rd/nanovg}"
+   "${workspace_loc:/${ProjName}/awtk/3rd/nanovg/base}"
+   "${workspace_loc:/${ProjName}/awtk/3rd/gpinyin/include}"
+   "${workspace_loc:/${ProjName}/awtk/3rd/libunibreak}"
+   "${workspace_loc:/${ProjName}/awtk-port}"
+   "${workspace_loc:/${ProjName}/pxp}"
+   ```
+
+2. 直接修改AWorks工程的.cproject文件，找到所有 name="Cross ARM C Compiler"或者name="Cross ARM C++ Compiler"的tool元素，在name="Include paths (-I)"的option子元素中加入：
 
 ```
 <listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk}&quot;"/>
-<listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk-port}&quot;"/>
-<listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk-demos}&quot;"/>
 <listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk/src}&quot;"/>
 <listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk/src/ext_widgets}&quot;"/>
 <listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk/3rd}&quot;"/>
+<listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk/3rd/agge}&quot;"/>
 <listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk/3rd/nanovg}&quot;"/>
 <listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk/3rd/nanovg/base}&quot;"/>
-<listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk/3rd/agge}&quot;"/>
 <listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk/3rd/gpinyin/include}&quot;"/>
 <listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk/3rd/libunibreak}&quot;"/>
+<listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/awtk-port}&quot;"/>
 <listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/pxp}&quot;"/>
-<listOptionValue builtIn="false" value="&quot;${workspace_loc:/${ProjName}/app}&quot;"/>
 ```
+
+## 二、注意事项
+
+  AWorks工程的*.ld文件切忌随意修改，这可能会导致内存读写异常（比如读写速度严重变慢）。
--- copy_files.py
+++ copy_files.py
@@ -6,17 +6,20 @@ import copy
 import shutil
 import platform
 
-def joinPath(root, subdir):
-  return os.path.normpath(os.path.join(root, subdir))
 
 # XXX: make sure no no ascii chars in the path name.
 
-SRC_ROOT_DIR=os.getcwd();
-DST_ROOT_DIR='D:\\work\\m1052_board\\broad';
+AWTK_ROOT_DIR = 'D:/z_lab';
+PORT_ROOT_DIR = os.getcwd();
+DST_ROOT_DIR = 'D:/z_lab/awtk-aworks-rt1052/broad';
 
-def copyFile(src, dst):
-    s = joinPath(SRC_ROOT_DIR, src)
-    d = joinPath(DST_ROOT_DIR, dst)
+
+def joinPath(root, subdir):
+  return os.path.normpath(os.path.join(root, subdir))
+
+def copyFile(src_root_dir, src, dst_root_dir, dst):
+    s = joinPath(src_root_dir, src)
+    d = joinPath(dst_root_dir, dst)
     print(s + '->' + d)
     dir=os.path.dirname(d)
     if os.path.exists(dir):
@@ -25,36 +28,48 @@ def copyFile(src, dst):
         os.makedirs(dir)
         shutil.copyfile(s, d)
 
-def copyFiles(src, dst):
-    s = joinPath(SRC_ROOT_DIR, src)
-    d = joinPath(DST_ROOT_DIR, dst)
+def copyAwtkFile(src, dst):
+    copyFile(AWTK_ROOT_DIR, src, DST_ROOT_DIR, dst)
+
+def copyPortFile(src, dst):
+    copyFile(PORT_ROOT_DIR, src, DST_ROOT_DIR, dst)
+
+def copyFiles(src_root_dir, src, dst_root_dir, dst):
+    s = joinPath(src_root_dir, src)
+    d = joinPath(dst_root_dir, dst)
     print(s + '->' + d)
     shutil.rmtree(d, True)
-    shutil.copytree(s, d)
+    shutil.copytree(s, d, ignore=shutil.ignore_patterns("*.obj"))
 
-copyFiles('awtk-port', 'awtk-port')
-copyFiles('awtk/3rd/stb', 'awtk/3rd/stb')
-copyFiles('awtk/3rd/libunibreak', 'awtk/3rd/libunibreak')
+def copyAwtkFiles(src, dst):
+    copyFiles(AWTK_ROOT_DIR, src, DST_ROOT_DIR, dst)
 
-copyFiles('awtk/3rd/agge', 'awtk/3rd/agge')
-copyFiles('awtk/3rd/nanovg/base', 'awtk/3rd/nanovg/base')
-copyFiles('awtk/3rd/nanovg/agge', 'awtk/3rd/nanovg/agge')
-copyFiles('awtk/3rd/gpinyin/src', 'awtk/3rd/gpinyin/src')
-copyFiles('awtk/3rd/gpinyin/include', 'awtk/3rd/gpinyin/include')
+def copyPortFiles(src, dst):
+    copyFiles(PORT_ROOT_DIR, src, DST_ROOT_DIR, dst)
 
-copyFiles('awtk/src/misc', 'awtk/src/misc')
-copyFiles('awtk/src/xml', 'awtk/src/xml')
-copyFiles('awtk/src/base', 'awtk/src/base')
-copyFiles('awtk/src/font', 'awtk/src/font')
-copyFiles('awtk/src/blend', 'awtk/src/blend')
-copyFiles('awtk/src/ui_loader', 'awtk/src/ui_loader')
-copyFiles('awtk/src/image_loader', 'awtk/src/image_loader')
-copyFiles('awtk/src/ext_widgets', 'awtk/src/ext_widgets')
-copyFiles('awtk/src/widget_animators', 'awtk/src/widget_animators')
+copyPortFiles('awtk-port', 'awtk-port')
+copyAwtkFiles('awtk/3rd/stb', 'awtk/3rd/stb')
+copyAwtkFiles('awtk/3rd/libunibreak', 'awtk/3rd/libunibreak')
 
-copyFile('awtk/src/awtk.c', 'awtk/src/awtk.c')
-copyFile('awtk/src/awtk.h', 'awtk/src/awtk.h')
-copyFile('awtk/src/vgcanvas/vgcanvas_nanovg.c', 'awtk/src/vgcanvas/vgcanvas_nanovg.c')
+copyAwtkFiles('awtk/3rd/agge', 'awtk/3rd/agge')
+copyAwtkFiles('awtk/3rd/nanovg/base', 'awtk/3rd/nanovg/base')
+copyAwtkFiles('awtk/3rd/nanovg/agge', 'awtk/3rd/nanovg/agge')
+copyAwtkFiles('awtk/3rd/gpinyin/src', 'awtk/3rd/gpinyin/src')
+copyAwtkFiles('awtk/3rd/gpinyin/include', 'awtk/3rd/gpinyin/include')
+
+copyAwtkFiles('awtk/src/misc', 'awtk/src/misc')
+copyAwtkFiles('awtk/src/xml', 'awtk/src/xml')
+copyAwtkFiles('awtk/src/base', 'awtk/src/base')
+copyAwtkFiles('awtk/src/font', 'awtk/src/font')
+copyAwtkFiles('awtk/src/blend', 'awtk/src/blend')
+copyAwtkFiles('awtk/src/ui_loader', 'awtk/src/ui_loader')
+copyAwtkFiles('awtk/src/image_loader', 'awtk/src/image_loader')
+copyAwtkFiles('awtk/src/ext_widgets', 'awtk/src/ext_widgets')
+copyAwtkFiles('awtk/src/widget_animators', 'awtk/src/widget_animators')
+
+copyAwtkFile('awtk/src/awtk.c', 'awtk/src/awtk.c')
+copyAwtkFile('awtk/src/awtk.h', 'awtk/src/awtk.h')
+copyAwtkFile('awtk/src/vgcanvas/vgcanvas_nanovg.c', 'awtk/src/vgcanvas/vgcanvas_nanovg.c')
 
 LCD_FILES=['lcd_mem.h', 
     'lcd_mem.inc', 
@@ -66,37 +81,37 @@ LCD_FILES=['lcd_mem.h',
     'lcd_mem_bgr565.c']
 for f in LCD_FILES:
     filename=os.path.join('awtk/src/lcd/', f);
-    copyFile(filename, filename)
+    copyAwtkFile(filename, filename)
 
 INPUT_METHODS_FILES=['input_engines/input_engine_pinyin.cpp',
         'input_methods/input_method_creator.c',
         'input_methods/input_method_default.inc',
-		'input_methods/input_method_null.inc',
+        'input_methods/input_method_null.inc',
         'input_methods/suggest_words.inc']
 for f in INPUT_METHODS_FILES:
     filename=os.path.join('awtk/src/', f);
-    copyFile(filename, filename)
+    copyAwtkFile(filename, filename)
 
 MAIN_LOOP_FILES=['main_loop_simple.h', 'main_loop_simple.c', 'main_loop_raw.inc']
 for f in MAIN_LOOP_FILES:
     filename=os.path.join('awtk/src/main_loop', f);
-    copyFile(filename, filename)
+    copyAwtkFile(filename, filename)
 
 
 WINDOW_ANIMATORS_FILES=['bottom_to_top.inc', 'top_to_bottom.inc', 'center_scale.inc', 'vtranslate.inc',
 'common.inc', 'window_animator_fb.c', 'fade.inc', 'htranslate.inc']
 for f in WINDOW_ANIMATORS_FILES:
     filename=os.path.join('awtk/src/window_animators', f);
-    copyFile(filename, filename)
+    copyAwtkFile(filename, filename)
 
 DEMO_FILES=['assets.h', 'assets.c', 'demo_main.c', 'demo_ui_app.c', 'common.inc']
 for f in DEMO_FILES:
     sfilename=os.path.join('awtk/demos', f);
     dfilename=os.path.join('awtk-demos/', f);
-    copyFile(sfilename, dfilename)
+    copyAwtkFile(sfilename, dfilename)
 
-copyFiles('awtk/demos/assets', 'awtk-demos/assets')
+copyAwtkFiles('awtk/demos/assets', 'awtk-demos/assets')
 
-copyFiles('pxp', 'pxp')
-# copyFile('user_code/main.c', 'awtk-demos/main.c')	
+copyPortFiles('pxp', 'pxp')
+# copyPortFile('user_code/main.c', 'awtk-demos/main.c')	
 
