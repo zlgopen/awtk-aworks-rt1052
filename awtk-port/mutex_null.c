5429d9153c84eaa9a4e5e96c4e007beb51555f11
chentan <chentan@zlg.cn>
2018-11-14 10:36:29 +0800

    add aworks mutex impl

--- awtk-port/mutex_null.c
+++ awtk-port/mutex_null.c
@@ -19,30 +19,48 @@
  *
  */
 
+#include "aw_sem.h"
 #include "base/mem.h"
 #include "base/mutex.h"
 
 struct _tk_mutex_t {
-  uint32_t none;
+  AW_MUTEX_DECL(__lock);
 };
 
 static tk_mutex_t s_tk_mutex_null;
 
 tk_mutex_t* tk_mutex_create() {
-  return &s_tk_mutex_null;
+  tk_mutex_t* mutex = TKMEM_ALLOC(sizeof(tk_mutex_t));
+  if (mutex) {
+  	AW_MUTEX_INIT(mutex->__lock, AW_SEM_INVERSION_SAFE);
+  }
+  return mutex;
 }
 
 ret_t tk_mutex_lock(tk_mutex_t* mutex) {
-  (void)mutex;
-  return RET_OK;
+	assert(mutex);
+	aw_err_t err = AW_MUTEX_LOCK( mutex->__lock, AW_SEM_WAIT_FOREVER );
+
+  switch (err) {
+    case AW_OK: return RET_OK;
+    default: return RET_FAIL;
+  }
 }
 
 ret_t tk_mutex_unlock(tk_mutex_t* mutex) {
-  (void)mutex;
-  return RET_OK;
+	assert(mutex);
+	aw_err_t err = AW_MUTEX_UNLOCK( mutex->__lock );
+
+  switch (err) {
+    case AW_OK: return RET_OK;
+    default: return RET_FAIL;
+  }
 }
 
 ret_t tk_mutex_destroy(tk_mutex_t* mutex) {
-  (void)mutex;
+	assert(mutex);
+	AW_MUTEX_TERMINATE( mutex->__lock );
+
+	TKMEM_FREE(mutex);
   return RET_OK;
 }
