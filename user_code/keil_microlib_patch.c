da63a7a25c13b0832e4a20ef404c85e6f157af2e
chentan <chentan@zlg.cn>
2019-07-10 12:40:53 +0800

    add keil microlib patch

---
+++ user_code/keil_microlib_patch.c
@@ -0,0 +1,28 @@
+#include <wchar.h>
+#include <time.h>
+#include "tkc/wstr.h"
+#include "tkc/time_now.h"
+
+size_t wcslen(const wchar_t* s) {
+  return wcs_len(s);
+}
+
+int wcscmp(const wchar_t* s1, const wchar_t* s2) {
+  return wcs_cmp(s1, s2);
+}
+
+wchar_t* wcscpy(wchar_t* s1, const wchar_t* s2) {
+  return wcs_cpy(s1, s2);
+}
+
+wchar_t* wcschr(const wchar_t* s, wchar_t c) {
+  return (wchar_t*)wcs_chr(s, c);
+}
+
+wchar_t* wcsdup(const wchar_t* s) {
+  return wcs_dup(s);
+}
+
+time_t time(time_t* t) {
+  return time_now_s();
+}
